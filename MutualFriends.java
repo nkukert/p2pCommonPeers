ce163e5d34635061ac67edd125aaac33ebed1441
nkukert <nikolas.kukert@ndsu.edu>
2019-11-14 11:39:21 -0600

    pushing functioning MutualFriends.java

--- MutualFriends.java
+++ MutualFriends.java
@@ -1,110 +1,95 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-
-import org.apache.hadoop.conf.Configuration;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+ 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Mapper.Context;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
 
 
 public class MutualFriends {
-	public static class Map extends Mapper<LongWritable, Text, Text, Text>{
+	public static class Map extends MapReduceBase
+                implements Mapper<LongWritable, Text, Text, Text>{
+                public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter)
+                        throws IOException{
+                        StringTokenizer tokenizer = new StringTokenizer(value.toString(), "\n");
+                        String line = null;
+                        String[] lineArray = null;
+                        String[] friendArray = null;
+                        String[] tempArray = null;
+                        while(tokenizer.hasMoreTokens()){
+                                line = tokenizer.nextToken();
+                                lineArray = line.split(" ", 2);
+                                friendArray = lineArray[1].split(" ");
+                                tempArray = new String[2];
+                                for(int i = 0; i < friendArray.length; i++){
+                                        tempArray[0] = friendArray[i];
+                                        tempArray[1] = lineArray[0];
+                                        Arrays.sort(tempArray);
+                                        output.collect(new Text(tempArray[0] + " " + tempArray[1]), new Text(lineArray[1]));
+                                }
+                        }
+                }
+        }
 		
-		private Text pair = new Text(); // type of output key
-		private Text List = new Text();
-
-		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			String[] line = value.toString().split(" ");
-			String User = line[0];
-			if (line.length ==2) {
-				ArrayList<String> FriendsList = new ArrayList<String>(Arrays.asList(line[1].split("\\,")));
-				for(String Friend:FriendsList){				
-					String FriendPair = (Integer.parseInt(User) < Integer.parseInt(Friend))?User+"\t"+Friend:Friend+"\t"+User;
-					ArrayList<String> temp = new ArrayList<String>(FriendsList);
-					temp.remove(Friend); 
-					String listString = String.join(",", temp);
-					pair.set(FriendPair);
-					List.set(listString);
-					context.write(pair,List);
+	public static class Reduce extends MapReduceBase
+		implements Reducer<Text, Text, Text, Text>{
+		public void reduce(Text key, Iterator<Text> values,
+		OutputCollector<Text, Text> output, Reporter reporter) throws IOException{
+				Text[] texts = new Text[2];
+				int index = 0;
+				while(values.hasNext()){
+						texts[index++] = new Text(values.next());
 				}
-			}
-			
-		}
-	}
-		
-	public static class Reduce extends Reducer<Text,Text,Text,Text> {
-		private Text result = new Text();
-		public String Mutual(String s1,String s2,int i) {
-			HashSet<String> map = new HashSet<String>();
-			String[] s1_split = s1.split("\\,");
-			String[] s2_split = s2.split("\\,");
-			String result = "";
-			for(String s:s1_split) {
-				map.add(s);
-			}
-			for(String s:s2_split) {
-				if (map.contains(s)){
-					result +=s+",";
+				String[] list1 = texts[0].toString().split(" ");
+				String[] list2 = texts[1].toString().split(" ");
+				List<String> list = new LinkedList<String>();
+				for(String friend1 : list1){
+						for(String friend2 : list2){
+								if(friend1.equals(friend2)){
+										list.add(friend1);
+								}
+						}
 				}
-			}
-			return result;	
+				StringBuffer sb = new StringBuffer();
+				for(int i = 0; i < list.size(); i++){
+						sb.append(list.get(i));
+						if(i != list.size() - 1)
+								sb.append(" ");
+				}
+				output.collect(key, new Text(sb.toString()));
 		}
-		
-		
-		public void reduce(Text key, Iterable<Text> values,Context context) throws IOException, InterruptedException {
-			String[] Friend_key_values = new String[2];
-			int i=0;
-			for(Text value:values){
-				Friend_key_values[i++] = value.toString();
-			}
-			
-			result.set(Mutual(Friend_key_values[0],Friend_key_values[1],i));
-			i++;
-			context.write(key,result);// create a pair <keyword, number of occurences>
-		}
-	}
+}
 		
 	public static void main(String[] args) throws Exception {
-		  	Configuration conf = new Configuration();
-		  	String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-		  	// get all args
-		  	if (otherArgs.length != 2) {
-		  	System.out.println(otherArgs[0]);
-		  	System.err.println("Usage: MutualFriends <in> <out>");
-		  	System.exit(2);
-		  	}
-		  	// create a job with name "Mutual Friends"
-		  	Job job = new Job(conf, "MutualFriends");
-		  	job.setJarByClass(MutualFriends.class);
-		  	job.setMapperClass(Map.class);
-		  	job.setReducerClass(Reduce.class);
-		  	// uncomment the following line to add the Combiner job.setCombinerClass(Reduce.class);
-		  	// set output key type
-		  	job.setOutputKeyClass(Text.class);
-		  	// set output value type
-		  	job.setOutputValueClass(Text.class);
-		  	//set the HDFS path of the input data
-		  	FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-		  	// set the HDFS path for the output
-		  	FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-		  	//Wait till job completion
-		  	System.exit(job.waitForCompletion(true) ? 0 : 1);
+		  	JobConf conf = new JobConf(MutualFriends.class);
+		  	conf.setJobName("Friend");
+ 
+			conf.setMapperClass(Map.class);
+			conf.setReducerClass(Reduce.class);
+ 
+			conf.setMapOutputKeyClass(Text.class);
+			conf.setMapOutputValueClass(Text.class);
+ 
+			conf.setOutputKeyClass(Text.class);
+			conf.setOutputValueClass(Text.class);
+ 
+			FileInputFormat.setInputPaths(conf, new Path(args[0]));
+			FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+ 
+			JobClient.runJob(conf);
 	}	
 
 }
